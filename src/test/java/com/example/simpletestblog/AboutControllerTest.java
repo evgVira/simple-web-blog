408552ec4676fb49d350468b14d327b24b6a66a6
evgen <eviriasovv@gmail.com>
2024-02-04 17:41:00 +0300

    add simple test

---
+++ src/test/java/com/example/simpletestblog/AboutControllerTest.java
@@ -0,0 +1,24 @@
+package com.example.simpletestblog;
+
+import com.example.simpletestblog.controllers.AboutController;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@WebMvcTest(AboutController.class)
+public class AboutControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testAboutPage() throws Exception{
+        mockMvc.perform(get("/about"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("about"));
+    }
+}
